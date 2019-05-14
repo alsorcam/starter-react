460b19d89c408160b67ce8fd79e064bc90a42298
asornosa <asornosa@iti.es>
2019-05-14 18:16:21 +0200

    feat(modal): Developing modal functionality

---
+++ src/demo/ModalDialog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ModalDialog() {
+  return (
+    <div className="demo-section">
+      <h2>Modal/Dialog</h2>
+      <div className="d-flex">
+        <button className="btn btn-sm btn-info">Launch modal</button>
+      </div>
+    </div>
+  );
+}
+
+export default ModalDialog;
--- src/shared/services/i18n.js
+++ src/shared/services/i18n.js
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import es from './assets/i18n/es.json';
-import en from './assets/i18n/en.json';
+import es from '../../assets/i18n/es.json';
+import en from '../../assets/i18n/en.json';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
