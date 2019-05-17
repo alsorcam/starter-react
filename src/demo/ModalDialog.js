d9ddf166f0b65f7973438ece19b464a5026b99e7
asornosa <asornosa@iti.es>
2019-05-17 14:02:49 +0200

    feat(modal): First approach for semantic ui modal

--- src/demo/ModalDialog.js
+++ src/demo/ModalDialog.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ModalBootstrap from '../shared/components/Modal-Bootstrap';
+import ModalSemanticUI from '../shared/components/Modal-SemanticUI';
 
 export default class ModalDialog extends React.Component {
 
   constructor(props) {
     super(props);
     this.handleBootstrap = this.handleBootstrap.bind(this);
+    this.handleSemanticUI = this.handleSemanticUI.bind(this);
     this.cancelModal = this.cancelModal.bind(this);
     this.acceptModal = this.acceptModal.bind(this);
 
     this.state = {
-      showBootstrap: false
+      showBootstrap: false,
+      showSemanticUI: false
     };
   }
 
@@ -18,12 +21,16 @@ export default class ModalDialog extends React.Component {
     this.setState({ showBootstrap: !this.state.showBootstrap });
   }
 
+  handleSemanticUI() {
+    this.setState({ showSemanticUI: !this.state.showSemanticUI });
+  }
+
   cancelModal() {
-    this.setState({ showBootstrap: false });
+    this.setState({ showBootstrap: false, showSemanticUI: false });
   }
 
   acceptModal() {
-    this.setState({ showBootstrap: false });
+    this.setState({ showBootstrap: false, showSemanticUI: false });
   }
 
   render() {
@@ -31,10 +38,13 @@ export default class ModalDialog extends React.Component {
       <div className="demo-section">
         <h2>Modal/Dialog</h2>
         <div className="d-flex">
-          <button className="btn btn-sm btn-info" onClick={this.handleBootstrap}>Boostrap</button>
-          <ModalBootstrap header="Header" body="Modal body" show={this.state.showBootstrap}
-            onCancel={this.cancelModal} onAccept={this.acceptModal} />
+          <button className="btn btn-sm btn-info mr-1" onClick={this.handleBootstrap}>Boostrap</button>
+          <button className="btn btn-sm btn-info mr-1" onClick={this.handleSemanticUI}>Semantic UI</button>
         </div>
+        <ModalBootstrap header="Bootstrap Header" body="Bootstrap Modal body" show={this.state.showBootstrap}
+          onCancel={this.cancelModal} onAccept={this.acceptModal} />
+        <ModalSemanticUI header="Semantic UI Header" body="Semantic UI Modal body" show={this.state.showSemanticUI}
+          onCancel={this.cancelModal} onAccept={this.acceptModal} />
       </div>
     );
   }
